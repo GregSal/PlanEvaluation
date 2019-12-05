d1db0ec0e8623e0c944ce1f846bbc4f8045661a2
Greg Salomons <gs13@queensu.ca>
2019-12-05 15:10:53 -0500

    updated directories

--- UpdateReports.py
+++ UpdateReports.py
@@ -1,49 +1,105 @@
 #!/usr/bin/env python
 from pathlib import Path
-from typing import Any, Dict, Tuple, List
-import xml.etree.ElementTree as ET
 import PySimpleGUI as sg
 from build_plan_report import load_config, update_reports
+''' Change the directories for DVH files, Report files , Save location
+  <DefaultDirectories>
+    <DVH>..\DVH Files</DVH>
+    <DVH_File>plan.dvh</DVH_File>
+    <ReportDefinitions>
+      <Directory>.\Data</Directory>
+    </ReportDefinitions>
+    <ReportTemplates>.\Data</ReportTemplates>
+    <ReportPickleFile>.\Data\Reports.pkl</ReportPickleFile>
+    <Save>..\Output</Save>
+  </DefaultDirectories>
+  '''
+#%% Main Menu
+def main_menu():
+    menu_def = [['&File', ['&DVH File Location', '&Save Location',
+                           '&Report Location', '&Update Report Definitions',
+                           'E&xit']]]
+    return sg.Menu(menu_def, tearoff=False, pad=(20, 1))
 
-'''
-    Update the Report Definition Pickle file
-'''
+def dir_selection_window(current_dir: Path,
+                         header = 'Select a Folder:',
+                         title = 'Select a directory')->Path:
+    '''Generate the window used to select directories.
+    '''
+    form_rows = [[sg.Text(header)],
+                 [sg.InputText(key='SelectedDir'),
+                  sg.FolderBrowse(target='SelectedDir',
+                                  initial_folder=str(current_dir))],
+                 [sg.Ok(), sg.Cancel()]
+                ]
+    window = sg.Window(title, form_rows)
+    event, values = window.read()
+    window.close()
+    if event in 'Cancel':
+        return current_dir
+    elif event in OK:
+        new_dir = Path(values['SelectedDir'])
+        return new_dir
+    return None
 
-def report_dir_list(base_path: Path, report_dirs):
+
+
+def save_dir_selection_window(current_dir: Path)->sg.Window:
+    '''Generate the window used to select directories containing report
+    definitions.
+    '''
+    form_rows = [[sg.Text('Select a Folder Containing Plan DVH files')],
+                 [sg.InputText(key='PlanDir'),
+                  sg.FolderBrowse(target='PlanDir',
+                                  initial_folder=str(current_dir))],
+                 [sg.Ok(), sg.Cancel()]
+                ]
+    window = sg.Window('Select the directory containing Plan DVH files', form_rows)
+    event, values = window.read()
+    window.close()
+    if event in 'Cancel':
+        return current_dir
+    elif event in OK:
+        new_dir = Path(values['PlanDir'])
+        return new_dir
+    return None
+
+
+locations = {
+    'DVH File Location': dict(
+        header = 'Select a Folder Containing Plan DVH files',
+        title = 'Plan DVH Directory'),
+    'Save Location': dict(
+        header = 'Select the directory to save the Plan Evaluation Report',
+        title = 'Save Location'),
+    'Report Location': dict(
+        header = 'Select the directory containing the Report definitions',
+        title = 'Report Location')
+    }
+locations_list = list(locations.keys())
+
+
+def get_report_dir_list(default_directories: ET.Element,
+                        base_str: str = '')->List[str]:
     '''Generate a string list of directories containing report definitions.
     The list is obtained from the Config .xml file.
     The directories in the string list replace the top directory path,
     given by base_str, with: ".\".
     Arguments:
-        report_dirs {List[Path, str]} -- List of report directory paths.
-        base_path {Path} -- A path to the top directory referenced.
+        default_directories {ET.Element} -- Sub-Element of the Config .xml file
+            containing the default directory paths.
+        base_str {str} -- A string path to the top directory referenced.
     '''
-    base_str = str(base_path)
     report_locations = list()
-    for location in report_dirs:
-        report_dir = Path(location).resolve()
+    report_path_element = default_directories.find('ReportDefinitions')
+    for location in report_path_element.findall('Directory'):
+        report_dir = Path(location.text).resolve()
         report_dir_str = str(report_dir).replace(base_str, '.')
         report_locations.append(report_dir_str)
     return report_locations
 
-def get_report_dir_list(config: ET.Element,
-                        base_str: Path = '')->List[str]:
-    '''Generate a string list of directories containing report definitions.
-    The list is obtained from the Config .xml file.
-    The directories in the string list replace the top directory path,
-    given by base_str, with: ".\".
-    Arguments:
-        config {ET.Element} -- Config .xml file Element.
-        base_str {str} -- A string path to the top directory referenced.
-    '''
-    default_directories = config.find(r'./DefaultDirectories')
-    rpt_dir = default_directories.find('ReportDefinitions')
-    report_dirs = [Path(dr.text).resolve()
-                   for dr in rpt_dir.findall('Directory')]
-    return report_dirs
 
-
-def selection_window(report_locations: List[str], base_str: str = '')->sg.Window:
+def selection_window(report_locations: list[str], base_str: str = '')->sg.Window:
     '''Generate the window used to select directories containing report
     definitions.
     '''
@@ -68,8 +124,7 @@ def selection_window(report_locations: List[str], base_str: str = '')->sg.Window
     return window
 
 
-def select_locations(report_locations, base_path):
-    base_str = str(base_path)
+def select_locations(report_locations, base_str):
     window = selection_window(report_locations, base_str)
     done=False
     dir_list = window['ReportDirs']
@@ -101,26 +156,6 @@ def select_locations(report_locations, base_path):
     return report_locations
 
 
-
-def update_report_definitions(config, base_path, report_locations=None):
-    '''Define Folder Paths, load report and plan data.
-    '''
-    default_directories = config.find(r'./DefaultDirectories')
-    pickle_file = Path(default_directories.findtext('ReportPickleFile'))
-    if not report_locations:
-         report_dirs = get_report_dir_list(config)
-         report_list = report_dir_list(base_path, report_dirs)
-    else:
-        report_list = report_dir_list(base_path, list(report_locations))
-    new_report_locations = select_locations(report_list, base_path)
-    if new_report_locations:
-        report_paths = [Path(dir).resolve() for dir in new_report_locations]
-        report_definitions = update_reports(config, report_paths, pickle_file)
-    else:
-        report_definitions = None
-    return report_definitions
-
-
 #%% Main
 def main():
     '''Define Folder Paths, load report and plan data.
@@ -132,17 +167,15 @@ def main():
     config = load_config(base_path, config_file)
     default_directories = config.find(r'./DefaultDirectories')
     pickle_file = Path(default_directories.findtext('ReportPickleFile'))
-    report_dirs = get_report_dir_list(config)
-    report_list = report_dir_list(base_path, report_dirs)
-    new_report_locations = select_locations(report_list, base_path)
     report_locations = get_report_dir_list(base_str, default_directories)
     report_locations = select_locations(report_locations, base_str)
-    if new_report_locations:
-        report_paths = [Path(dir).resolve() for dir in new_report_locations]
+    if report_locations:
+        report_paths = [Path(dir).resolve() for dir in report_locations]
         report_definitions = update_reports(config, report_paths, pickle_file)
     else:
         report_definitions = None
-    return report_definitions
+
+
 
 
 
